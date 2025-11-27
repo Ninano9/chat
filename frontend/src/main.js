0c35d6b28d08dc2571e5be4f6f8c4c5e89e0cfb5
nkm <km.nam@okestro.com>
2025-11-27 15:40:48 +0900

    프론트 db연결

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001'
 
 const app = createApp(App)
 const pinia = createPinia()
